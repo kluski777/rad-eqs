280012a9b6c755840056b9f0d1b616a2b7b543e8
Marcin Wójcik <kluski777@gmail.com>
2023-08-29 17:34:49 +0200

    wszystko jest źle

--- readEQS.py
+++ readEQS.py
@@ -7,10 +7,10 @@ path2pao = "/home/stud/praktyki/Data_analysis/pierre_auger/"
 path2pao += "scalers.csv"
 defFormt = "%Y-%m-%dT%H:%M:%S.%fZ"
 
-def sec2date(arg, formatS=defFormt):
-    """ conversion from seconds to date (in string) """
-    return datetime.fromtimestamp(int(arg)).strftime(formatS)
-
+# def sec2date(arg, formatS=defFormt):
+#     """ conversion from seconds to date (in string) """
+#     return datetime.fromtimestamp(int(arg)).strftime(formatS)
+#
 
 def date2sec(arg, formatS=defFormt):
     """ convert date (in string) to seconds """
@@ -26,7 +26,7 @@ for row in eqInfo: # tutaj się czyta od najnowszych do najstarszych odwrotnie j
     timeEQs.append(int(date2sec(row['time'])))
     mag.append(float(row['mag']))
     
-mag = mag[::-1]
+mag = mag[::-1] # odwracam arraya bo się jakoś od tyłu czytają z tego pliku
 timeEQs = timeEQs[::-1]
     
 #### Koniec czytania trzęsień ziemi teraz czytanie pierre augiera    
@@ -40,39 +40,41 @@ for row in auger:
 timeEQs = np.array(timeEQs)
 timeAug = np.array(timeAug)
 
-def calcCorr(days, minutes):
-    shift = int(60*60*24*days) # 1 dzień shifta do przodu dla Augiera
-    global timeAug
-    timeAug = timeAug + shift
+def calcCorr(shift, minutes):
+    global timeAug # jak tego uniknąć ten array jest ogromny?
+    timeAug = timeAug + shift # ta linijka jest do bani
     
     binWidth = int(minutes*60) # szerokość bina w sekundach
     minTime = np.maximum(np.min(timeAug), np.min(timeEQs))
     maxTime = np.minimum(np.amax(timeAug), np.amax(timeEQs))
-    maxTime += maxTime%binWidth # musi być podzielny przez minutes żeby liczba okien była całkowita
-    augWin = [] # okienko czasowe dla augera, zawiera liczbe zarejestrowanych promieniowań
-    eqsWin = [] # okienko dla trzęsień ziemi, największe trzęsienia ziemi w okienkach.
-    temp = 0
-    oldTemp = 0
+    # maxTime += maxTime%binWidth
+    augWin = [] # liczba zarejstrowanych promieniowań w okienku czasowym
+    eqsWin = [] # Największa magnituda w danym okienku.
+    temp, oldTemp = 0, 0 # indeks dolny i górny
     maxBinVal = minTime + binWidth
+    oldVal = 0
     
     while minTime < maxTime:
-        augWin.append(np.count_nonzero((minTime <= timeAug) & (timeAug < maxBinVal)))
-        temp += np.count_nonzero((minTime <= timeEQs) & (timeEQs < maxBinVal))
-        if oldTemp != temp:
+        temp += np.count_nonzero((minTime <= timeEQs) & (timeEQs < maxBinVal)) # chyba tą linijkę można poprawić żeby przyspieszyć skrypt
+        if temp != oldTemp:
             eqsWin.append(np.amax(mag[oldTemp:temp]))
-        else:
-            eqsWin.append(0) # w sumie to to jest nieprawda najlepiej byłoby to 0 pominąć, ale rozmiary muszą się zgadzać dlatego muszę je zostawić
+            augWin.append(np.count_nonzero((minTime <= timeAug) & (timeAug < maxBinVal)) - oldVal)
+        oldVal = np.count_nonzero((minTime <= timeAug) & (timeAug < maxBinVal))
         oldTemp = temp
         maxBinVal += binWidth
         minTime += binWidth
         
     return (np.corrcoef(augWin, eqsWin))[0,1] # np.corrcoef returns a matrix [0][1] points out to corr(A,B) not corr(A,A) nor corr(B,B)      
 
-days = np.linspace(4, 6, num=21) # shift of Auger data forward in time in days,
-binWidth = np.linspace(105, 110, num=51) # binWidth in minutes
-coef = np.zeros([len(days),len(binWidth)])
+days_start, days_stop = 0, 1
+timeAug += days_start*24*60*60 # jak zaczynamy badań próbkę od dnia days_start to przesuwamy to
+step = 0.01 # w dniach
+length = int((days_stop-days_start)/step)
+binWidth = np.linspace(1,150, num=150) # w minutach
+coef = np.zeros([length,len(binWidth)])
+step = int(step*24*60*60) # zamiana jednostek na sekundy
 
-for i in range(len(days)):
+for i in range(length):
     for j in range(len(binWidth)):
         print(f"Starting loop for: day = {i} day and binWidth = {binWidth[j]}")
-        coef[i,j] = calcCorr( days[i], binWidth[j] )
+        coef[i] = calcCorr(step, binWidth[j])
