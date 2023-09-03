4b26b9dfc37015a61fe1b4866a6856a1bc4f9f81
Marcin Wójcik <kluski777@gmail.com>
2023-09-03 19:19:11 +0200

    Liczenie korelacji

--- readAll.py
+++ readAll.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from sys import path
 from math import factorial
 
-path2EQ, path2CR, path2Sun = '/home/stud/praktyki/Data_analysis/EQs/wwEQ.csv', '/home/stud/praktyki/Data_analysis/pierre_auger/scalers.csv', '/home/stud/praktyki/Data_analysis/sunspots/SIDC.csv'
+path2EQ, path2CR, path2Sun = '/home/stud/praktyki/Data_analysis/EQs/query.csv', '/home/stud/praktyki/Data_analysis/pierre_auger/scalers.csv', '/home/stud/praktyki/Data_analysis/sunspots/SIDC.csv'
 path += [path2EQ, path2CR, path2Sun]
 EQDateFormat = "%Y-%m-%dT%H:%M:%S.%fZ"
 
@@ -108,41 +108,76 @@ def makeWin42vars(EQ, CR, binWidth, startTime, stopTime):
     indexes = ~np.isnan(toRetCR + toRetEQ)
     return toRetEQ[indexes], toRetCR[indexes]
 
+def dayShiftProbability(startDay, stopDay, shiftStop):
+    P = 1675*24*60*60
+    d = 5*24*60*60 # length of a window over which the sum / mean value will be calculated
+    shift = np.arange(startDay,stopDay,shiftStop)
+    res = np.zeros(len(shift))
+    czas = EQData.timeRecorded # co ciekawe mają inne adresy
+    
+    for i in range(len(res)):
+        EQData.timeRecorded = czas + shift[i]*24*60*60 # shifting EQ data
+        
+        binStarts   = np.arange(date2sec('2014-04-02T22:07:12.00Z'), EQData.timeRecorded[-1] - P, d)
+        binEnds     = binStarts + P
+        print(f"{i/len(res)*100}% completed\n", end = '\r', flush=True)
+        probability, times = [], []
+        
+        for j,l in zip(binStarts,binEnds):
+            sumMag, meanCR = makeWin42vars(EQData, CRData, d, j, l)
+            if np.median(sumMag) != 0 and np.median(meanCR) != 0:
+                c = np.where((sumMag/np.median(sumMag)-1)*(meanCR/np.median(meanCR)-1) > 0, 1, 0)
+                probability.append(Pcdf(len(c), sum(c)))
+                times.append(sec2fraction(l)) # tylko po to żeby wyświetlać lata na wykresie
+        
+        res[i] = np.min(probability[:-75])
+    
+    
+    plt.title("Minimum probabilities with dependency\nto the number of days EQ data was shifted")
+    plt.plot(shift, np.log10(res))
+    plt.xlabel("Number of days that EQ data was shifted")
+    plt.ylabel(r"$log_{10}(P_{cdf})$")
+
 SunData = SunClass(path2Sun)
 EQData = EQClass(path2EQ)
 CRData = CRClass(path2CR)
 
+# dobra tu się zaczynamy korelacją pearsona zajmować, to bardzo proste będzie
 
-# wykres 2 i 3
-P = 1675*24*60*60
-d = 5*24*60*60 # length of a window over which the sum / mean value will be calculated
-shift = np.arange(10,20,0.5)
-res = np.zeros(len(shift))
-czas = EQData.timeRecorded # co ciekawe mają inne adresy
+def PearsonCorr(daysBack):
+    meanRateCorrs = medianRateCorrs = magnitudes = []
 
-for i in range(len(res)):
-    EQData.timeRecorded = czas + shift[i]*24*60*60 # shifting EQ data
-    
-    binStarts   = np.arange(date2sec('2014-04-02T22:07:12.00Z'), EQData.timeRecorded[-1] - P, d)
-    binEnds     = binStarts + P
-    print(f"{i/len(res)*100}% completed\n", end = '\r', flush=True)
-    probability, times = [], []
-    stopTime    = CRData.timeRecorded[-1]
-    
-    for j,l in zip(binStarts,binEnds):
-        sumMag, meanCR = makeWin42vars(EQData, CRData, d, j, l)
-        if np.median(sumMag) != 0 and np.median(meanCR) != 0:
-            c = np.where((sumMag/np.median(sumMag)-1)*(meanCR/np.median(meanCR)-1) > 0, 1, 0)
-            probability.append(Pcdf(len(c), sum(c)))
-            times.append(sec2fraction(l)) # tylko po to żeby wyświetlać lata na wykresie
-    
-    res[i] = np.min(probability[:-75])
+    for i in range(len(EQData.timeRecorded)):
+        rateCorrs = CRData.rateCorr[((CRData.timeRecorded < EQData.timeRecorded[i]) & (CRData.timeRecorded > EQData.timeRecorded[i] - daysBack))]
+        if len(rateCorrs) != 0:
+            magnitudes.append(EQData.mag[i])
+            meanRateCorrs.append(np.mean(rateCorrs))
+            medianRateCorrs.append(np.median(rateCorrs))
+        
+    print(f"Długość danych: {len(magnitudes)}")
+    return np.corrcoef(meanRateCorrs, magnitudes)[0,1], np.corrcoef(medianRateCorrs, EQData.mag)[0,1]
 
 
-plt.title("Minimum probabilities with dependency\nto the number of days EQ data was shifted")
-plt.plot(shift, np.log10(res))
-plt.xlabel("Number of days that EQ data was shifted")
-plt.ylabel(r"$log_{10}(P_{cdf})$")
+Range = np.arange(.1,3,.1)
+Range *= 24*60*60
+arrLength = len(Range)
+meanCorr, medianCorr = np.zeros(arrLength), np.zeros(arrLength)
+
+
+# for i in range(arrLength):
+#     meanCorr[i], medianCorr[i] = PearsonCorr(Range[i])
+
+meanRateCorrs, medianRateCorrs, magnitudes = [], [], []
+
+
+for i in range(len(EQData.timeRecorded)):
+    rateCorrs = CRData.rateCorr[((CRData.timeRecorded < EQData.timeRecorded[i]) & (CRData.timeRecorded > EQData.timeRecorded[i] - 5*24*60*60))]
+    if len(rateCorrs) != 0:
+        magnitudes.append(EQData.mag[i])
+        meanRateCorrs.append(np.mean(rateCorrs))
+        medianRateCorrs.append(np.median(rateCorrs))
+
+
 
 
 # """ #Poniżej wykres 1 """
